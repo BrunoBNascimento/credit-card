6daffb7d600b19178a80ca2ad07f52bc29486c67
Bruno Benicio <bruno.benicio@mercadolivre.com>
2022-03-01 11:59:43 -0300

    [Chore] add isPublic on bills controller

--- src/bill/bill.controller.ts
+++ src/bill/bill.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { IsPublic } from 'src/auth/is-public.decorator';
 import BillFilterDTO from './types/bill-filter.dto';
 
+@IsPublic()
 @ApiTags('Bills')
 @ApiBearerAuth('JWT')
 @Controller('bill')
